b462df3b1a9f502d1f25957bf2ebb597d7d77d0f
slizer98 <erick.m.villa.r@gmail.com>
2023-03-25 09:17:00 -0600

    agregando lista de gastos, problema en el modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Header from './components/Header'
 import Modal from './components/Modal'
+import ListadoGastos from './components/ListadoGastos'
+import { generarId } from './helpers'
 import IconNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
@@ -9,45 +11,66 @@ function App() {
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
   const [gastos, setGastos] = useState([])
+  const listaRef = useRef(null)
 
   const handleNuevoGasto = () => {
+    window.scrollTo(0, 0)
     setModal(!modal)
-
+    document.body.classList.add('fijar')
     setTimeout(() => {
       setAnimarModal(!animarModal)
+      document.body.classList.remove('fijar')
     }, 500);
   }
 
   const guardarGasto = gasto => {
+    gasto.id = generarId()
+    gasto.fecha = Date.now()
     setGastos([...gastos, gasto])
+
+    setAnimarModal(!animarModal)
+    setTimeout(() => {
+      setModal(!modal)
+      document.body.classList.remove('fijar')
+      if(listaRef.current){
+        listaRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' })
+      }
+    }, 500);
   }
   
   return (
-    <>
+    <div> 
       <Header 
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
-      />
+        />
       {isValidPresupuesto && (
-      <div className='nuevo-gasto'>
-        <img 
-          src={IconNuevoGasto} 
-          alt="Icono nuevo gasto" 
-          onClick={handleNuevoGasto}
-          />
-      </div>
+        <>
+          <main ref={listaRef}>
+            <ListadoGastos gastos={gastos} />
+          </main>
+          <div className='nuevo-gasto'>
+            <img 
+              src={IconNuevoGasto} 
+              alt="Icono nuevo gasto" 
+              onClick={handleNuevoGasto}
+              />
+          </div>
+        </>
       )}
       {modal && (
-        <Modal 
+        <div className="modal fijar">
+          <Modal 
           setModal={setModal}
           animarModal={animarModal}
           setAnimarModal={setAnimarModal}
           guardarGasto={guardarGasto}
-        />
-      )}
-    </>
+          />
+        </div>
+        )}
+    </div>
   )
 }
 
